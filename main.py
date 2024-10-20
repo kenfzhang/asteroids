bc37a35fa74a7874348a7d158bfa0a923931bd8c
kenfzhang <4808683+kenfzhang@users.noreply.github.com>
2024-10-20 13:55:40 -0700

    implement turning in player.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print("Screen width: "+str(SCREEN_WIDTH))
     print("Screen height: "+str(SCREEN_HEIGHT))
@@ -13,7 +17,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick / 1000
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
